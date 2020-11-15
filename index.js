7a7c53b6e843750d5ce0f0367f003f74d110e62b
JossinAntony <jossinde@gmail.com>
2020-11-15 09:49:42 +0100

    post method for employee details defined.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
-
+const bodyparser = require('body-parser');
 const app = express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
 
 app.get('/', (req, res)=>{
     res.send('Welcome');
+});
+
+app.post('/employee_details', (req, res)=>{
+    var getName = req.body.emp_name;
+    var getId = req.body.emp_id;
+    var getCode = req.body.emp_code;
+    var getDesig = req.body.emp_desig;
+    var getSalary = parseInt(req.body.emp_salary);
+    var getAdrs = req.body.emp_adrs;
+    var getPhone = req.body.emp_phone;
+
+    res.status(200).send({
+        'name': getName,
+        'id': getId,
+        'code': getCode,
+        'desig': getDesig,
+        'salary': getSalary,
+        'adrs': getAdrs,
+        'phone': getPhone,
+    });
 });
 
 app.listen(3000, (e)=>{
